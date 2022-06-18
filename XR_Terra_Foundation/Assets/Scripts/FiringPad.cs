be8ca434186da942967885de81c5a25baedad053
ET <earnestwt3@gmail.com>
2022-06-18 13:43:12 -0500

    Begin Pair-Programming 3

---
+++ XR_Terra_Foundation/Assets/Scripts/FiringPad.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringPad : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public Transform spawnpoint;
+    public float projectileForce; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player triggered this firing pad.");
+            GameObject spawnedProjectile = Instantiate(projectilePrefab, spawnpoint.position, spawnpoint.rotation);
+            spawnedProjectile.GetComponent<Rigidbody>().AddForce(spawnedProjectile.transform.forward * projectileForce, ForceMode.VelocityChange); 
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
