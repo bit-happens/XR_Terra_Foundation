38b2e9f7d8666bde447829a8b2d0c91fb2ae5a02
ET <earnestwt3@gmail.com>
2022-06-24 07:41:33 -0500

    Begin Pair Program Red Light Green Light Scene

---
+++ XR_Terra_Foundation/Assets/Scripts/RedLightGreenLight.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class RedLightGreenLight : MonoBehaviour
+{
+    public float speed;
+    public bool canMove;
+    public bool wonGame;
+    public float minRedTime, maxRedTime, minGreenTime, maxGreenTime;
+    Vector3 movement; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+        wonGame = false;
+
+        StartCoroutine(StopLightTimer());
+    }
+
+    public void OnMovement(InputAction.CallbackContext context) 
+    {
+        Vector2 givenMovement = context.ReadValue<Vector2>();
+        movement = new Vector3(givenMovement.x, 0, givenMovement.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += movement * speed * Time.deltaTime; 
+    }
+
+    IEnumerator StopLightTimer()
+    {
+        while(!wonGame)
+        {
+            if (!canMove)
+            {
+                yield return new WaitForSeconds(Random.Range(minRedTime, maxRedTime));
+                canMove = true;
+            }
+
+            else
+            {
+                yield return new WaitForSeconds(Random.Range(minGreenTime, maxGreenTime));
+                canMove = false;
+            }
+        }
+    }
+
+}
