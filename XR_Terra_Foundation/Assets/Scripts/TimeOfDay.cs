30883862b5c7eaea6ea9dd71ed95bf8853268ee0
ET <earnestwt3@gmail.com>
2022-06-25 17:13:12 -0500

    Working project with locomotion and interactive UI

--- XR_Terra_Foundation/Assets/Scripts/TimeOfDay.cs
+++ XR_Terra_Foundation/Assets/Scripts/TimeOfDay.cs
@@ -18,7 +18,7 @@ public class TimeOfDay : MonoBehaviour
     public void RotateSun(Slider passedSlider)
     {
         float percent = (passedSlider.value - passedSlider.maxValue) / (passedSlider.maxValue - passedSlider.minValue);
-        Debug.Log("percent has a value of: " + percent);
+        // Debug.Log("percent has a value of: " + percent);
 
         directionalLight.color = lightColor.Evaluate(percent);
         directionalLight.intensity = intensityCurve.Evaluate(percent); 
