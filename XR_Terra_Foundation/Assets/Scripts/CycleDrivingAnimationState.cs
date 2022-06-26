8ce66b919591e11a7d9c2267535b6339a42a7222
ET <earnestwt3@gmail.com>
2022-06-25 19:31:41 -0500

    Add car animation and UI driving toggle button. Working final draft!

---
+++ XR_Terra_Foundation/Assets/Scripts/CycleDrivingAnimationState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleDrivingAnimationState : MonoBehaviour
+{
+    Animator anim;
+    bool isDriving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        isDriving = false;
+        
+    }
+
+    public void ToggleDriving()
+    {
+        isDriving = !isDriving;
+        anim.SetBool("isDriving", isDriving);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
